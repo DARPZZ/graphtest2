7bce398c0a54abfe2dda112c07f245d8a9087b62
Rasmus <rasmustofthermansen@gmail.com>
2024-04-08 22:12:01 +0200

    Updated to take json from the server. Added support for motor degreese

--- start.py
+++ start.py
@@ -3,36 +3,47 @@ import matplotlib.animation as animation
 import numpy as np
 from websocket import create_connection
 import threading
+import json
 
-# Data
-fig, ax = plt.subplots(figsize=(8, 4), subplot_kw=dict(polar=True))
-
+fig, ax = plt.subplots(figsize=(15, 10), subplot_kw=dict(polar=True))
 theta = np.linspace(0, np.pi, 50, endpoint=False)
-width = np.pi / 50  
-
+width = np.pi / 50
 ax.set_theta_direction(-1)
 ax.set_theta_offset(np.pi)
 ax.set_xticklabels([])
 values = []
 
-ws = create_connection("ws://10.176.69.179:6969")
+ws = create_connection("ws://192.168.9.119:6969")
 
 def read_from_websocket():
     global values
+    degree = 2.0
     while True:
         message = ws.recv()
-        print(f"Received: {message}")
-        values.append(float(message)) 
-        
+        data = json.loads(message)
+        print(f"Received: {data}")
+        if data['type'] == 'degree':
+            print(f"Motor position: {data['value']} degrees")
+            degree = data['value']
+            if degree >= 170:
+                values.clear()
+        elif data['type'] == 'distance':
+            print(f"Distance: {data['value']} cm")
+            distance = data['value']
+            values.append((degree, distance))
+
+
 threading.Thread(target=read_from_websocket, daemon=True).start()
 
 def update(num):
     ax.clear()
-    ax.bar(theta[:len(values)], values, width=width, bottom=0.0)
+    for value in values:
+        degree, distance = value
+        ax.bar(np.deg2rad(degree), distance, width=width, bottom=0.0)
     ax.set_theta_direction(-1)
     ax.set_theta_offset(np.pi)
     ax.set_xticklabels([])
+    ax.set_ylim(0,250)
 
-ani = animation.FuncAnimation(fig, update, frames=range(50), repeat=True, interval=500)
+ani = animation.FuncAnimation(fig, update, frames=range(50), repeat=True)
 plt.show()
-
